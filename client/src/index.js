bb65bb0130059177a8fe65739c7a467eeb1c4646
maturapruk <toey.2549@hotmail.com>
2018-04-27 14:23:55 +0700

    client now can delete bear

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,21 @@
-+var express = require('express'); 
-+var app = express(); 
-+var router = express.Router(); 
-+var bodyParser = require('body-parser'); 
-+var cors = require('cors');
-+  
-+var bears = [
-+    { id: 1, name: 'Ple' },
-+    { id: 2, name: 'Tum' }
-+]; 
-+  
-+router.route('/bears') 
-+    .get(function(req, res) {
-+        res.send(bears);
-+    })
-+    .post(function(req, res) { 
-+        var bear = {}; 
-+        bear.name = req.body.name; 
-+        bears.push(bear); 
-+        res.json({ message: 'Bear created!' }); 
-+    }); 
-+
-+app.use(cors());
-+// all of our routes will be prefixed with /api 
-+app.use('/api', bodyParser.json(), router); 
-+app.listen(8000); 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import { fetchBear } from './actions';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+let store = createStoreWithMiddleware(reducers)
+store.dispatch(fetchBear());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('root'));
